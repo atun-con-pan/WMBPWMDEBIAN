c46502d049bb6cbe6c965c9ea9d449576a7f149d
Felipe <abelardofelipe722@gmail.com>
2024-07-24 09:52:07 -0600

    Update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -10,14 +10,14 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 ## Launch
 
-polybar date -c ~/.config/polybar/current.ini &
-polybar ram -c ~/.config/polybar/current.ini &
-polybar ssd -c ~/.config/polybar/current.ini &
-polybar cpu -c ~/.config/polybar/current.ini &
-polybar volume -c ~/.config/polybar/current.ini &
-polybar battery -c ~/.config/polybar/current.ini &
-polybar wireless -c ~/.config/polybar/current.ini &
-polybar ip -c ~/.config/polybar/current.ini &
+polybar date -c ~/.config/bspwm/polybar/current.ini &
+polybar ram -c ~/.config/bspwm/polybar/current.ini &
+polybar ssd -c ~/.config/bspwm/polybar/current.ini &
+polybar cpu -c ~/.config/bspwm/polybar/current.ini &
+polybar volume -c ~/.config/bspwm/polybar/current.ini &
+polybar battery -c ~/.config/bspwm/polybar/current.ini &
+polybar wireless -c ~/.config/bspwm/polybar/current.ini &
+polybar ip -c ~/.config/bspwm/polybar/current.ini &
 
 ## Center bar
-polybar primary -c ~/.config/polybar/workspace.ini &
+polybar primary -c ~/.config/bspwm/polybar/workspace.ini &
